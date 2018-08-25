e31773d3172f8906b126561bb0a7c0815c05e8ce
Faizan <mirzafaizanejaz@gmail.com>
2018-08-25 21:17:12 +0500

    Initial comit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Faizan's First React native App</Text>
+        <Text>I'm Just testing this out`</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
