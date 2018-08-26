0603da8a9bd6a28a8f7d06ebd3104dd0d5a1a145
Faizan <mirzafaizanejaz@gmail.com>
2018-08-26 15:11:46 +0500

    starting to change toolbar

--- src/screens/List/Toolbar.js
+++ src/screens/List/Toolbar.js
@@ -42,7 +42,7 @@ class Toolbar extends PureComponent {
         <View>
           <Row style={styles.toolbarContainer}>
             <View style={styles.titleContainer}>
-              <Text style={styles.titleText}>My Checks</Text>
+              <Text style={styles.titleText}>Mirpur Farm House</Text>
             </View>
             <View style={styles.menuIconContainer}>
               <Ionicons name="md-menu" size={24} color="#008dff" />
